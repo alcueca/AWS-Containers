b669657ec6d7417256440c5fed514794a95fbe06
Alberto Cuesta Cañada <mail@albertocuesta.es>
2018-06-16 15:17:04 +0100

    Changed export names to allow multiple solutions in parallel

--- CreateContainersBase.sh
+++ CreateContainersBase.sh
@@ -3,11 +3,11 @@
 PATH=$PATH:/home/alberto/AWS/Git/AWS-Tooling/
 
 aws cloudformation create-stack \
---stack-name NetworkAndSecurityStack \
---template-body file://NetworkAndSecurity.yaml \
+--stack-name ContainersBaseStack \
+--template-body file://ContainersBase.yaml \
 --region eu-west-1 \
 --profile alberto \
 --capabilities CAPABILITY_IAM
 
-waitForStatus.py -s NetworkAndSecurityStack -r CREATE_COMPLETE
+waitForStatus.py -s ContainersBaseStack -r CREATE_COMPLETE
 beep
--- DeleteContainersBase.sh
+++ DeleteContainersBase.sh
@@ -3,7 +3,7 @@
 PATH=$PATH:/home/alberto/AWS/Git/AWS-Tooling/
 
 aws cloudformation delete-stack \
-    --stack-name NetworkAndSecurityStack
-waitForDeletion.py -s NetworkAndSecurityStack
+    --stack-name ContainersBaseStack
+waitForDeletion.py -s ContainersBaseStack
 beep
 
